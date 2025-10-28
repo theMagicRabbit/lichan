ecc94c12efb121c2669171706d6f29d2891cc906
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-10-28 11:50:34 -0500

    Add handler to download and print games

---
+++ handlers.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func (s *state) handlerDownloads(username string) error {
+	opts := "opening=true"
+	reqUrl := fmt.Sprintf("%s%s%s?%s", s.ApiUrl, "/api/games/user/", username, opts)
+
+	if s.Config.LastGameTime > 0 {
+		reqUrl = fmt.Sprintf("%s&since=%d", reqUrl, s.Config.LastGameTime)
+	}
+
+	fmt.Println(reqUrl)
+	req, err := http.NewRequest("GET", reqUrl, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("authorization", fmt.Sprintf("Bearer %s", s.Config.PAT))
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	var games []Game
+	gamesScaner := bufio.NewScanner(res.Body)
+
+	for gamesScaner.Scan() {
+		gameBytes := gamesScaner.Bytes()
+		game := Game{}
+		err = json.Unmarshal(gameBytes, &game)
+		if err != nil {
+			log.Printf("Error unmarshaling game: %v\n", err)
+			continue
+		}
+		games = append(games, game)
+	}
+
+	err = gamesScaner.Err()
+	if err != nil {
+		log.Printf("Error scanning for games: %v\n", err)
+	}
+
+	for _, game := range games {
+		gamePGN, err := GameToPGN(game, s.SiteUrl)
+		if err != nil {
+			log.Printf("Error converting game to PGN: %v\n", err)
+			continue
+		}
+		fmt.Println("========== GAME ==========")
+		fmt.Println(gamePGN)
+	}
+
+	return nil
+}
