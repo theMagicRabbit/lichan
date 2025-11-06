62882e95ed8d364b2dcad9b8d1e83267ed02b874
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-11-06 10:55:52 -0600

    Test tokenizer
    Spoiler alert, the issue was not in the tokenizer, but maybe this will
    become useful later?

--- game_test.go
+++ game_test.go
@@ -4,6 +4,47 @@ import (
 	"testing"
 )
 
+func TestMoveTokenizer(t *testing.T) {
+	tests := []struct{
+		Input struct{
+			String string
+			AtEnd bool
+		}
+		Expected struct{
+			Advance int
+			Token string
+			Err error
+		}
+	}{
+		{
+			Input: struct{String string; AtEnd bool}{
+				String: "O-O-O",
+				AtEnd: false,
+			},
+			Expected: struct{Advance int; Token string; Err error}{
+				Advance: 5,
+				Token: "O-O-O",
+				Err: nil,
+			},
+		},
+	}
+	for _, test := range tests {
+		advance, token, err := tokenizerMoveString([]byte(test.Input.String), test.Input.AtEnd)
+		if err != test.Expected.Err {
+			t.Errorf("Error %v does not match expected: %v\n", err, test.Expected.Err)
+			break
+		}
+		if advance != test.Expected.Advance {
+			t.Errorf("Advance bytes: %v does not match expected: %v\n", advance, test.Expected.Advance)
+			break
+		}
+		if string(token) != test.Expected.Token {
+			t.Errorf("Result: %v does not match expected: %v\n", string(token), test.Expected.Token)
+			break
+		}
+	}
+}
+
 func TestParseMoveString(t *testing.T) {
 	tests := []struct{
 		Input string
