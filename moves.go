1f49d8fbc7814e8d28e8b027f35bc735fe1e0a8f
Benjamin Toll <themagicrabbit3of6@gmail.com>
2025-11-09 22:21:07 -0600

    Add outline of move parser

--- moves.go
+++ moves.go
@@ -1,14 +1,21 @@
 package main
 
+import "strings"
 
-//func (GS *GameState) ApplyMove(move string, turn PlayerColor) (*GameState, error) {
-//	for _, boardPiece := range GS.Pieces {
-////		if boardPiece.PlayerColor != turn {
-//			continue
-//		}
-////		if boardPiece.PieceType != pieceType {
-//			continue
-////		}
-//	}
-//	
-//}
+
+func (GS *GameState) ApplyMove(ms string, turn PlayerColor) (*GameState, error) {
+	move, err := ParseMoveString(strings.TrimSpace(ms))
+	if err != nil {
+		return nil, err
+	}
+
+	for _, boardPiece := range GS.Pieces {
+		if boardPiece.PlayerColor != turn {
+			continue
+		}
+		if boardPiece.PieceType != move.PieceType {
+			continue
+		}
+	}
+	return nil, nil
+}
